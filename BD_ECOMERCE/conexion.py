1ab3157c850c21a6c890e781ba367666f7255db9
Reexxiis <rexis.meta27@gmail.com>
2023-02-01 17:32:45 -0800

    cambio 2

---
+++ BD_ECOMERCE/conexion.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import Error
+
+class connection():
+
+    def __init__(self):
+        try:
+            self.conexion = mysql.connector.connect(
+                host = 'localhost',
+                port = 3306,
+                user = 'root',
+                password = '',
+                db = 'tecnicos'
+            )
+        except Error as ex:
+            print("Error durante la Conexion: {0}".format(ex))
+
+
+    def lista_tecnicos(self):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                cursor.execute("SELECT * FROM tecnico ORDER BY ASC")
+                resultado = cursor.fetchall()
+                return resultado
+            except Error as ex:
+                print("Error al intentar la conexion.")
